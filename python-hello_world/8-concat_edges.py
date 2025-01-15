c695dd8ab37bbafd1d421fce9b60cce92c151e04
jmitchell35 <julian.m.mitchell@gmail.com>
2025-01-15 18:08:00 +0100

    Fixing task 6, file 8 output

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(str[str.find("object"):str.find("programming") + len("programming")] + " with " + str[:6])
+print(str[39:67] + str[107:112] + str[:6])
+print()
