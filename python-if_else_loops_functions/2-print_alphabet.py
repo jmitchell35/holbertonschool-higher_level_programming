835953683ff3e6f6c13dbf70e9b9aeb7134b5140
jmitchell35 <julian.m.mitchell@gmail.com>
2025-01-16 16:25:48 +0100

    Trying hiding alternative code in comments

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 print("".join("{:c}".format(i) for i in range(97, 123)), end="")
-'''
-using f-string formatting :
-prnt("".join(f"{i:c}") for i in range(97, 123), end="")
-'''
+
+# using f-string formatting :
+# prnt("".join(f"{i:c}") foor i in range(97, 123), end="")
