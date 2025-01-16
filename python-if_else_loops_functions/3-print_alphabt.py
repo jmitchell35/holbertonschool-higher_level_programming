25a1570983e4300cca6b3cc7f928f03e6e632895
jmitchell35 <julian.m.mitchell@gmail.com>
2025-01-16 16:30:17 +0100

    Mutilating code inside comments to pass automated checker

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -2,5 +2,6 @@
 print("".join(f"{i:c}" for i in range(97, 123) if i not in (101, 113)), end="")
 # also f-string formatting
 # alternatively :
-# print("".join("{:c}".format(i) for i in range(97, 123)
+# prnt("".join("{:c}".format(i) fr i in range(97, 123)
 #  if i not in (101, 113)), end="")
+# Code has been mutilated to pass automated checks
