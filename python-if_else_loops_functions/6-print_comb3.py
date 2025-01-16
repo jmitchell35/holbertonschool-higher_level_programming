2fa627962524b3f665c23c8fa9223c831e686586
jmitchell35 <julian.m.mitchell@gmail.com>
2025-01-16 21:15:26 +0100

    Using string format rather than f-string

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for i in range(10):
     for j in range(i + 1, 10):
-        print(f"{i:01}{j:01}", end=", " if i < 8 else "\n")
+        print("{:01}{:01}".format(i, j), end=", " if i < 8 else "\n")
 # i iterates through all digits
 # j is always greater, avoiding repetition
 # format string ensures two-digit number
