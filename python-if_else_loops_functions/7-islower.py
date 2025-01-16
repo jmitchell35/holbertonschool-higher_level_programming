cb805bdeb0b41d4fc2463a3120c5208822dab2fd
jmitchell35 <julian.m.mitchell@gmail.com>
2025-01-16 21:19:04 +0100

    pycodestyle-compliant task 7

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 def islower(c):
-    if ord(c) >=97 and ord(c) <= 122: # easy condition
-        return True # watch for capital letter
+    if 97 <= ord(c) <= 122:  # easy condition, see inequation
+        return True  # watch for capital letter
     else:
         return False
 # output is handled inside testing main function
-# e.g.: print("a is {}".format("lower" if islower("a") else "upper"))
+# e.g.: prnt("a is {}".format("lower" if islower("a") else "upper"))
