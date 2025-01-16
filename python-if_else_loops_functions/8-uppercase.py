5dd1f6b99dccdba1d70ba30ab5a6860a97821b78
jmitchell35 <julian.m.mitchell@gmail.com>
2025-01-16 21:26:21 +0100

    Fixing format, limiting use of print function

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def uppercase(str):
+    upper_str = ""
     for char in str:
         if ord('a') <= ord(char) <= ord('z'):  # Checks if the character is lowercase
         # Python allows bounding ("surounding") a number in such inequation
-            print(chr(ord(char) - 32), end="") # Converts to uppercase, prints
+            upper_str += chr(ord(char) - 32)  # Converts to uppercase, prints
         else:
-            print(char, end="")  # Print non-lowercase characters as is
-    print()  # Prints a newline after the string
+            upper_str += char  # Print non-lowercase characters as is
+    print("{}".format(upper_str))  # Prints a newline after the string
