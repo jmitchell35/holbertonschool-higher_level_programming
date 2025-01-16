2198149f261b4acdfc52dba0d533145af0788938
jmitchell35 <julian.m.mitchell@gmail.com>
2025-01-16 16:12:48 +0100

    Test with Axel

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,2 @@
 #!/usr/bin/python3
-print("".join("{:c}".format(i) for i in range(97, 123)), end="")
-'''
-using f-string formatting :
-print("".join(f"{i:c}" for i in range(97, 123)), end="")
-'''
+print("".join.("{:c}".format(i) for i in range(97, 123)), end="")
