e70809612c9c3a0de20fcf909c50f812474907f1
jmitchell35 <julian.m.mitchell@gmail.com>
2025-01-16 16:05:33 +0100

    Trying changing f-string to string format

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 print("".join("{:c}".format(i) for i in range(97, 123)), end="")
-# using f-string formatting
-# print("".join(f"{i:c}" for i in range(97, 123)), end="")
+'''
+using f-string formatting :
+print("".join(f"{i:c}" for i in range(97, 123)), end="")
+'''
