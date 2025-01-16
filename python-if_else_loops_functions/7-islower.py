48ea93af71eb7d050085b9a8b39bc61b5ea0925e
jmitchell35 <julian.m.mitchell@gmail.com>
2025-01-16 21:22:25 +0100

    Fixing first line requirement

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,8 +1,6 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 def islower(c):
-    if 97 <= ord(c) <= 122:  # easy condition, see inequation
+    if 97 <= ord(c) <= 122:  # easy condition, notice inequation
         return True  # watch for capital letter
     else:
         return False
-# output is handled inside testing main function
-# e.g.: prnt("a is {}".format("lower" if islower("a") else "upper"))
