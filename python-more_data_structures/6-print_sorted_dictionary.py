bb6dd6364357b6728a7f047018392394d9ac16e3
jmitchell35 <julian.m.mitchell@gmail.com>
2025-01-21 10:37:52 +0100

    Updating 6

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
+    if not a_dictionary:
+        return
     keys_list = a_dictionary.keys()
     keys_list = list(keys_list)
     keys_list.sort()
