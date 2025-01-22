5f4be3f2a8bcfe800c4c2bd36f79ba971aede61c
jmitchell35 <julian.m.mitchell@gmail.com>
2025-01-22 14:41:20 +0100

    task 0

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    printed_nb = 0
+    try:
+        for i in range(x):
+            print(my_list[i], end="")
+            printed_nb += 1
+
+    except IndexError:
+        pass
+
+    if printed_nb > 0:
+        print()
+    return printed_nb
