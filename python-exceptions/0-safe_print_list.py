26481723fda967f47f3b0b27afc050cc29a9c832
jmitchell35 <julian.m.mitchell@gmail.com>
2025-01-22 20:56:37 +0100

    task 0 reviewed once more, \n fixed, format modified

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,10 +3,11 @@ def safe_print_list(my_list=[], x=0):
     printed_nb = 0
     try:
         for i in range(x):
-            print(my_list[i], end="")
+            print("{}".format(int(my_list[i])), end="")
             printed_nb += 1
 
     except IndexError:
         pass
 
+    print()
     return printed_nb
