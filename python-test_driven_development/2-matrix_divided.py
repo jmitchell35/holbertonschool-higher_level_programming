2691000a95ee420b0d6126921e7676f4465ce117
jmitchell35 <julian.m.mitchell@gmail.com>
2025-01-24 18:02:28 +0100

    task 1

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -25,28 +25,28 @@ def matrix_divided(matrix, div):
         raise ZeroDivisionError("division by zero")
 
     if not isinstance(div, (int, float)):
-        raise TypeError ("div must be a number")
+        raise TypeError("div must be a number")
 
     if not isinstance(matrix, list):
-        raise TypeError ("matrix must be a matrix "
-            "(list of lists) of integers/floats")
+        raise TypeError("matrix must be a matrix "
+                "(list of lists) of integers/floats")
     new_matrix = []
     ref_row_len = 0
     for row in matrix:
         if not isinstance(row, list):
-            raise TypeError ("matrix must be a matrix "
-                "(list of lists) of integers/floats")
+            raise TypeError("matrix must be a matrix "
+                    "(list of lists) of integers/floats")
         new_row = []
         if not ref_row_len:
             ref_row_len = len(row)
         else:
             if len(row) != ref_row_len:
-                raise TypeError (
+                raise TypeError(
                         "Each row of the matrix must have the same size")
         for nb in row:
             if not isinstance(nb, (int, float)):
-                raise TypeError ("matrix must be a matrix "
-                    "(list of lists) of integers/floats")
+                raise TypeError("matrix must be a matrix "
+                        "(list of lists) of integers/floats")
             new_row.append(float(f"{nb / div:.2f}"))
         new_matrix.append(new_row)
     return new_matrix
