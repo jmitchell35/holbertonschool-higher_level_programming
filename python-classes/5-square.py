f173def683c9aae7059dfb3b4c66bdb51e6e9ebb
jmitchell35 <julian.m.mitchell@gmail.com>
2025-01-27 14:07:28 +0100

    task 5

---
+++ python-classes/5-square.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+"""
+Module providing simple Square class
+"""
+
+
+class Square():
+    def __init__(self, size=0):
+        """
+        Initialize a Square instance.
+
+        Args:
+            size (int, optional): Length of square's side. Defaults to 0.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """
+        Get the size of the square.
+
+        Returns:
+        int: The length of the square's side.
+        """
+        return self.__size
+    
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise TypeError ("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """
+        Calculate the area of the square.
+
+        Returns:
+            int: The square's area (size squared).
+
+        Example:
+            >>> square = Square(4)
+            >>> square.area()
+            16
+        """
+        return self.__size ** 2
+
+    def my_print():
+        """
+        Print the square using # characters.
+
+        Prints:
+            Empty line if size is 0
+            Square pattern of # characters otherwise
+
+        Example:
+            >>> square = Square(3)
+            >>> square.my_print()
+            ###
+            ###
+            ###
+        """
+        if self.__size == 0:
+            print()
+        else:
+            for _ in range(self.__size):
+                print("#" * self.__size)
