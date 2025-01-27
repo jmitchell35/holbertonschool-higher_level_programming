7125b5976c9ce20a99769a68a4ee4715b0a84424
jmitchell35 <julian.m.mitchell@gmail.com>
2025-01-27 14:00:21 +0100

    updating documentation for task 2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,6 +1,28 @@
 #!/usr/bin/python3
+"""
+Module provides the Square class
+"""
+
+
 class Square():
+    """
+    Simple Square class with initialization method including value verif
+    """
     def __init__(self, size=0):
+        """
+        Initialize a Square instance with input validation.
+
+        Args:
+        size (int, optional): Length of square's side. Defaults to 0.
+
+        Raises:
+        TypeError: If size is not an integer or is negative.
+
+        Example:
+        >>> square = Square(5)  # Valid
+        >>> square = Square(-1) # Raises TypeError
+        >>> square = Square("5") # Raises TypeError
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
