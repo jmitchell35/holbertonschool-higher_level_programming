bc48159ef726da7558ba43e55d5633250bc34e17
jmitchell35 <julian.m.mitchell@gmail.com>
2025-01-27 14:00:07 +0100

    updating documentation for task 1

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,4 +1,24 @@
 #!/usr/bin/python3
+"""
+Module provides the Square class
+"""
+
+
 class Square():
+    """
+    Simple class with __init__ method
+    """
     def __init__(self, size):
+        """
+        Initialize a Square instance.
+
+        Args:
+        size: Length of square's side. Must be a non-negative integer.
+
+        Returns:
+        None
+
+        Example:
+        >>> square = Square(5)
+        """
         self.__size = size
