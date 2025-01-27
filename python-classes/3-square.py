173657e24a724fc888d6ea6081f2dda429f8d2d4
jmitchell35 <julian.m.mitchell@gmail.com>
2025-01-27 14:39:50 +0100

    task 3 - pycodestyle-compliant

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -9,24 +9,24 @@ class Square():
     Simple Square class with initialization and area calculation methods
     """
     def __init__(self, size=0):
-    """
-    Initialize a Square instance with input validation.
+        """
+        Initialize a Square instance with input validation.
 
-    Args:
-        size (int, optional): Length of square's side. Defaults to 0.
+        Args:
+            size (int, optional): Length of square's side. Defaults to 0.
 
-    Raises:
-        TypeError: If size is not an integer or is negative.
+        Raises:
+            TypeError: If size is not an integer or is negative.
 
-    Example:
-        >>> square = Square(5)  # Valid
-        >>> square = Square(-1) # Raises TypeError
-        >>> square = Square("5") # Raises TypeError
-    """
+        Example:
+            >>> square = Square(5)  # Valid
+            >>> square = Square(-1) # Raises TypeError
+            >>> square = Square("5") # Raises TypeError
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
-            raise TypeError ("size must be >= 0")
+            raise TypeError("size must be >= 0")
         self.__size = size
 
     def area(self):
