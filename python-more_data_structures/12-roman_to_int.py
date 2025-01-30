d2e15aecb916b8aac531a667f49c7619e67c1c4a
jmitchell35 <julian.m.mitchell@gmail.com>
2025-01-30 14:31:29 +0100

    Code simplified

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,40 +1,29 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not roman_string or isinstance(roman_string, str) is False:
+    if not roman_string:
         return 0
-    decimal = 0
+
+    for char in roman_string:
+        if char not in 'IVXLCDM':
+            print("Le format du chiffre romain est incorrect")
+            return -1
+
+    roman_values = {
+    "I": 1,
+    "V": 5,
+    "X": 10,
+    "L" : 50,
+    "C": 100,
+    "D": 500,
+    "M": 1000,
+    }
+    number = 0
     for i in range(len(roman_string)):
-        if roman_string[i] == 'I':
-            if (i + 1 in range(len(roman_string)) and
-                    roman_string[i + 1] in 'VX'):
-                decimal -= 1
+        if not roman_values[roman_string[i]]:
+            return None
+        else:
+            if i + 1 in range(len(roman_string)) and roman_values[roman_string[i]] < roman_values[roman_string[i + 1]]:
+                number -= roman_values[roman_string[i]]
             else:
-                decimal += 1
-
-        if roman_string[i] == 'V':
-            decimal += 5
-
-        if roman_string[i] == 'X':
-            if (i + 1 in range(len(roman_string)) and
-                    roman_string[i + 1] in 'LC'):
-                decimal -= 10
-            else:
-                decimal += 10
-
-        if roman_string[i] == 'L':
-            decimal += 50
-
-        if roman_string[i] == 'D':
-            decimal += 500
-
-        if roman_string[i] == 'C':
-            if (i + 1 in range(len(roman_string)) and
-                    roman_string[i + 1] in 'DM'):
-                decimal -= 100
-            else:
-                decimal += 100
-
-        if roman_string[i] == 'M':
-            decimal += 1000
-
-    return decimal
+                number += roman_values[roman_string[i]]
+    return number
