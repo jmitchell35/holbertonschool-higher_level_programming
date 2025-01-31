1786b014831f71cd5789411df7d3c0e2fa8fc348
jmitchell35 <julian.m.mitchell@gmail.com>
2025-01-31 10:48:51 +0100

    pycodestyle-compliant task 3

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -47,7 +47,7 @@ class Rectangle:
             str: Rectangle pattern or empty string
         """
         return '\n'.join("#" * self.width for _ in range(self.height))\
-                if self.width != 0 and self.height != 0 else ""
+            if self.width != 0 and self.height != 0 else ""
 
     @property
     def width(self):
