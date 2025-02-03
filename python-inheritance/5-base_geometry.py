556f7aa66dc47491ef390ded7a0164a17d31a087
jmitchell35 <julian.m.mitchell@gmail.com>
2025-02-03 15:52:34 +0100

    task 5

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This module defines a base class for geometric shapes.
+"""
+
+
+class BaseGeometry:
+    """
+    A base class that serves as a template for geometric shape classes.
+    This class is currently empty and meant to be extended by other classes
+    that will implement specific geometric calculations and properties.
+    """
+    pass
