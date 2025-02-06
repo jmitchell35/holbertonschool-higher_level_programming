9a385af3045cc531ea6859dfc60b4d779ebd48dd
jmitchell35 <julian.m.mitchell@gmail.com>
2025-02-06 12:58:51 +0100

    Changed use of abs function

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -15,25 +15,25 @@ class Shape(ABC):
 
 class Circle(Shape):
     def __init__(self, radius):
-        self.radius = abs(radius)
+        self.radius = radius
 
     def area(self):
-        return pi*(self.radius**2)
+        return abs(pi * (self.radius**2))
 
     def perimeter(self):
-        return 2 * pi * self.radius
+        return abs(2 * pi * self.radius)
 
 
 class Rectangle(Shape):
     def __init__(self, width, height):
-        self.width = abs(width)
-        self.height = abs(height)
+        self.width = width
+        self.height = height
 
     def area(self):
-        return self.width * self.height
+        return abs(self.width * self.height)
 
     def perimeter(self):
-        return 2 * (self.width + self.height)
+        return abs(2 * (self.width + self.height))
 
 
 def shape_info(shape):
