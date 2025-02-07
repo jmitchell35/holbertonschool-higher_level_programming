140c0bda62647e47c72631a2a718425642c9f7a2
jmitchell35 <julian.m.mitchell@gmail.com>
2025-02-07 14:21:56 +0100

    Updating docstring as I noticed a mistake

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -7,8 +7,8 @@ This module defines a base class for geometric shapes.
 class BaseGeometry:
     """
     A base class that serves as a template for geometric shape classes.
-    This class is currently empty and meant to be extended by other classes
-    that will implement specific geometric calculations and properties.
+    This class is meant to be extended by other classes that will implement
+    specific geometric calculations and properties.
     """
     def area(self):
         """
