978751cabd9c0d1172fc86d6418ddd477e86c6a1
jmitchell35 <julian.m.mitchell@gmail.com>
2025-03-06 10:33:41 +0100

    Task 5

---
+++ SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,7 @@
+-- Task 4
+-- Write a script that creates the table unique_id on your MySQL server.
+-- Details inside README file
+CREATE TABLE IF NOT EXISTS unique_id (
+    id INT DEFAULT 1 UNIQUE,
+    name VARCHAR(256)
+);
