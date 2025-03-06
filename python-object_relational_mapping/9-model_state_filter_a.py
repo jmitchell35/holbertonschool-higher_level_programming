22ee5b75f8093dbddf6ab5fc39837744133f7aa2
jmitchell35 <julian.m.mitchell@gmail.com>
2025-03-06 13:29:22 +0100

    Task 9

---
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+Script that lists all State objects that contain the letter 'a'
+from the database hbtn_0e_6_usa.
+Takes 3 arguments: mysql username, mysql password, and database name.
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    """
+    Main function that will not execute when imported.
+    Lists all State objects containing the letter 'a'.
+    """
+    # Get MySQL connection parameters from command line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    # Create engine that connects to the MySQL server
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(username, password, db_name),
+                           pool_pre_ping=True)
+
+    # Create a configured "Session" class
+    Session = sessionmaker(bind=engine)
+
+    # Create a Session instance
+    session = Session()
+
+    # Query State objects containing the letter 'a' and sort by id
+    states = session.query(State).filter(
+        State.name.like('%a%')).order_by(State.id).all()
+
+    # Display the results
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
+
+    # Close the session
+    session.close()
