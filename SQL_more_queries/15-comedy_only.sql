5ef235b6571812cd4a31960fc0c7cb08f6d204dd
jmitchell35 <julian.m.mitchell@gmail.com>
2025-03-06 10:55:55 +0100

    Task 15

---
+++ SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,15 @@
+-- Task 15
+-- Write a script that lists all Comedy shows in the database hbtn_0d_tvshows.
+-- Details inside README file
+SELECT 
+    tv_shows.title
+FROM 
+    tv_shows
+JOIN 
+    tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+JOIN 
+    tv_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE 
+    tv_genres.name = 'Comedy'
+ORDER BY 
+    tv_shows.title ASC;
