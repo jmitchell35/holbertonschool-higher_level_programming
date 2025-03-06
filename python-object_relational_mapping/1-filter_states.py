343cc9795e88c693f5b661c6e5b6ea776bb7f505
jmitchell35 <julian.m.mitchell@gmail.com>
2025-03-06 12:52:45 +0100

    Avoiding injections ?

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -31,7 +31,8 @@ if __name__ == "__main__":
     cursor = db.cursor()
 
     # Execute the query to get states starting with uppercase 'N' ordered by id
-    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
+    query = "SELECT * FROM states WHERE name LIKE BINARY %s ORDER BY id ASC"
+    cursor.execute(query, ('N%',))
 
     # Fetch all the results
     states = cursor.fetchall()
