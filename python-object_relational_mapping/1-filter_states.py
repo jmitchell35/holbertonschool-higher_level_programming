f462872afdc3ce86256a7a8e4f339d8fe48c21e3
jmitchell35 <julian.m.mitchell@gmail.com>
2025-03-06 12:44:33 +0100

    Maybe pycodestyle compliant ?

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -4,7 +4,6 @@ Script that lists all states with a name starting with N (upper N)
 from the database hbtn_0e_0_usa.
 Takes 3 arguments: mysql username, mysql password and database name.
 """
-
 import MySQLdb
 import sys
 
