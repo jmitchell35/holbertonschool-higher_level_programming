e5b39b596a911216e470c2e4a1a8e5a51e99351f
jmitchell35 <julian.m.mitchell@gmail.com>
2025-03-06 13:36:34 +0100

    Model city

---
+++ python-object_relational_mapping/model_city.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Contains the class definition of a City
+"""
+from sqlalchemy import Column, Integer, String, ForeignKey
+from model_state import Base
+
+
+class City(Base):
+    """
+    City class that inherits from Base
+
+    Attributes:
+        id (int): Primary key for the city
+        name (str): Name of the city
+        state_id (int): Foreign key to states.id
+    """
+    __tablename__ = 'cities'
+
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
