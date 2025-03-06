c646dfd79e01158787ea79b3dd2f749d7c466f73
jmitchell35 <julian.m.mitchell@gmail.com>
2025-03-06 13:54:25 +0100

    Sticking more to SQLalchemy patterns

--- python-object_relational_mapping/100-relationship_states_cities.py
+++ python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,46 +1,27 @@
 #!/usr/bin/python3
 """
 Script that creates the State "California" with the City "San Francisco"
-in the database hbtn_0e_100_usa.
-Takes 3 arguments: mysql username, mysql password, and database name.
+from the database hbtn_0e_100_usa.
 """
 import sys
-from relationship_state import Base, State
-from relationship_city import City
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-
+from relationship_state import Base, State
+from relationship_city import City
 
 if __name__ == "__main__":
-    """
-    Main function that will not execute when imported.
-    Creates State "California" with City "San Francisco" using relationship.
-    """
-    # Get MySQL connection parameters from command line arguments
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
-
-    # Create engine that connects to the MySQL server
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(username, password, db_name))
-
-    # Create all tables in the engine
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
-    # Create a configured "Session" class
     Session = sessionmaker(bind=engine)
-
-    # Create a Session instance
     session = Session()
 
-    # Create a new State "California" and City "San Francisco"
-    california = State(name="California")
-    california.cities = [City(name="San Francisco")]
+    new_state = State(name="California")
+    new_city = City(name="San Francisco")
+    new_state.cities.append(new_city)
 
-    # Add the state to the session
-    session.add(california)
+    session.add(new_state)
     session.commit()
 
-    # Close the session
     session.close()
