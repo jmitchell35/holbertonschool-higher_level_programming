19ac08f5da8a349da1192cda8a0ebc48900cb168
jmitchell35 <julian.m.mitchell@gmail.com>
2025-04-10 14:23:38 +0200

    Task 1 - corrected header targeting

--- javascript-dom_manipulation/1-script.js
+++ javascript-dom_manipulation/1-script.js
@@ -1,3 +1,3 @@
 document.getElementById('red_header').addEventListener('click', () => {
-  document.getElementById('header').style.color='#ff0000';
+  document.querySelector('header').style.color = '#ff0000';
 });
